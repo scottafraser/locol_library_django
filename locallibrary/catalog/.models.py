d2aa14b31427961e28dd440fa6923552d6610876
Scott Fraser <scottafraser@yahoo.com>
2018-07-24 23:49:59 -0700

    language class

--- locallibrary/catalog/.models.py
+++ locallibrary/catalog/.models.py
@@ -16,7 +16,17 @@ class Genre(models.Model):
 
 from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 
-
+class Language(models.Model):
+    """
+    Model representing a Language (e.g. English, French, Japanese, etc.)
+    """
+    name = models.CharField(max_length=200, help_text="Enter a the book's natural language (e.g. English, French, Japanese etc.)")
+    
+    def __str__(self):
+        """
+        String for representing the Model object (in Admin site etc.)
+        """
+        return self.name
 
 class Book(models.Model):
     """
@@ -73,4 +83,29 @@ class BookInstance(models.Model):
         """
         String for representing the Model object
         """
-        return '{0} ({1})'.format(self.id,self.book.title)
+        
+class Author(models.Model):
+    """
+    Model representing an author.
+    """
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ["last_name","first_name"]
+    
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular author instance.
+        """
+        return reverse('author-detail', args=[str(self.id)])
+    
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return '{0}, {1}'.format(self.last_name,self.first_name
+
