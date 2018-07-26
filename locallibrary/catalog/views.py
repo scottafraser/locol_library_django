be1b68c79b77ac2a3e40317b2551c9561fa1ade0
Scott Fraser <scottafraser@yahoo.com>
2018-07-26 15:13:35 -0700

    finish author details page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -31,6 +31,15 @@ from django.views import generic
 
 class BookListView(generic.ListView):
     model = Book
+    paginate_by = 2
 
 class BookDetailView(generic.DetailView):
     model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
